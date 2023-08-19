31f840a7783ed8b9271b5c15f5d882d8a44f02f1
david <david.m.sellers@gmail.com>
2023-08-18 21:29:57 -0500

    more work on assign1

---
+++ Week1/Assignments/assignment1.rkt
@@ -0,0 +1,69 @@
+#lang racket
+
+
+(require rackunit)
+
+;;; Create 10 lats given 5 atoms
+'(ant bee cat dog eel)
+'(ant)
+'(bee)
+'(cat)
+'(ant bee)
+'(ant cat)
+'(ant dog)
+'(ant bee cat)
+'(ant cat dog)
+'(ant bee cat dog)
+'(ant cat dog eel)
+
+;;; Create lists using cons
+
+(define a 'all)
+(define b 'these)
+(define c 'problems)
+
+;;; Goals (all (these problems))
+(check-equal? (cons a (cons (cons b (cons c '())) '())) '(all (these problems)))
+
+
+;;; Goals (all (these) problems)
+(check-equal? (cons a (cons (cons b '()) (cons c  '()))) '(all (these) problems))
+
+;;; Goals ((all these) problems)
+(check-equal? (cons (cons a  (cons b '())) (cons c  '())) '((all these) problems))
+
+;;; Goals ((all) these problems)
+(check-equal? (cons (cons a '()) (cons b  (cons c  '()))) '((all) these problems))
+
+
+;;; (car (cons a l), a=french, l=(fries)
+(define a1 'french)
+(define l '(fries))
+(check-equal? (car (cons a1 l)) 'french)
+
+
+;;; Can (null? (cons a2 l)) #t
+(define a2 '())
+(null? (cons a2 '()))
+;;;No, let l be the empty list, then (cons a l) results in the non-empty list (a). All other list contain more elements than the empty list, thus they will still have more elements than the empty list after application of cons.
+
+
+;;; 5
+(null? (cdr '((meatballs))))
+
+;;; 6
+
+
+;;; 7
+(define atom?
+  (lambda (a)
+    (cond )))
+(define nonlat?
+  (lambda (l)
+    (cond 
+      (null? l #t)
+      (atom? (car l) #f)
+      (else nonlat? (cdr l)))))
+
+(nonlat? '(a b c))
+(nonlat? '((a) b c))
