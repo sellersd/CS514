5d0e7d1447421daa1563dcebf682ea2d3939a03a
David <david.m.sellers@gmail.com>
2023-09-05 21:03:43 -0500

    python implemenation?

--- Assignments/3/ribcage.py
+++ Assignments/3/ribcage.py
@@ -1,3 +1,6 @@
+def environ():
+   return [[],[]] 
+
 def isnull(x):
     if len(x) == 0:
         return True
@@ -12,21 +15,21 @@ def cons(atom, x):
     # list.append() always returns None
     # use list + list_item instead
     # print("consing")
-    # print(atom, x)
-    return ([atom] + x)
+    print(atom, x)
+    return atom + x
 
 def cdr(x):
     if not isnull(x):
-        return x[1:]
+        return x[1]
     else:
         return []
 
 def ribs(var, val):
     # print("ribsing")
-    return (cons(var, cons(val, [])))
+    return cons([var], cons([val], []))
 
 def empty(x):
-    if len(x) == 0:
+    if len(x[0]) == 0 and len(x[1]) == 0:
         return True
     else:
         return False
@@ -34,21 +37,23 @@ def empty(x):
 def ext_env(var, val, env):
     # print(var, val, env)
     if empty(env):
-        return cons(ribs(var, val), [])
+        # print("empty")
+        temp =  [cons(ribs(var, val), []),environ()]
+        # print('temp', temp)
+        return temp
     else:
-        cons(ext_env(var, val, cdr(env)), env)
+        # print("not empty")
+        # print('cdr', cdr(env))
+        return cons(env[0], ext_env(var, val, cdr(env)))
 
-# print(type([]))
-#
-# test = [1,2,3,4]
-# print(cdr(cdr(cdr(test))))
-# print("not empty",empty(cdr(cdr(cdr(test)))))
-# print("empty",empty(cdr(cdr(cdr(cdr(test))))))
-# print("cdr of []",empty(cdr(cdr(cdr(cdr(cdr(cdr(test))))))))
-# print()
 
-# print(ribs(3 , 4))
-print(ext_env(('a','b'), (3,4), []))
+
+print(ext_env(['a','b'], [3,4], environ()))
+test_env = ext_env(['a','b'], [3,4], environ())
+# print("test_env", test_env)
+test_env2 = ext_env(['c','d'], [5,6], test_env)
+print(test_env2)
+
 # print(ext_env(('a','b'), (3,4), 
 #               (ext_env(('a','b'), (3,4), 
 #                        ext_env(('a','b'), (3,4), [])))))
